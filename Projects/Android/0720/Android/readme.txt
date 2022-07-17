43fc656de4812d86ce810c21ca65f5f0a267e76b
yzhengk <41242069+yzhengk@users.noreply.github.com>
2022-07-16 23:47:02 -0400

    Initial commit

---
+++ Projects/Android/0720/Android/readme.txt
@@ -0,0 +1,11 @@
+adb logcat > logcat.txt
+clear logcat every time
+run the app on Android studio
+
+------
+Collecting the data
+------
+
+Close the app
+Close adb
+
---
+++ Projects/Android/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/android_native_app_glue/android_native_app_glue.o.d
@@ -0,0 +1,5 @@
+C:/Users/lab2/Downloads/Oculus/VrSamples/VrCubeWorld_NativeActivity/Projects/Android/build/intermediates/ndkBuild/debug/obj/local/arm64-v8a/objs-debug/android_native_app_glue/android_native_app_glue.o: \
+  C:\Users\lab2\AppData\Local\Android\Sdk\ndk\20.0.5594570\build\\..\sources\android\native_app_glue\android_native_app_glue.c \
+  C:\Users\lab2\AppData\Local\Android\Sdk\ndk\20.0.5594570\build\\..\sources\android\native_app_glue\android_native_app_glue.h
+
+C:\Users\lab2\AppData\Local\Android\Sdk\ndk\20.0.5594570\build\\..\sources\android\native_app_glue\android_native_app_glue.h:
